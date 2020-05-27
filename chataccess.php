eef5c6485585ad8bfab82514bdbceb1f96853c52
BastienHue <55254472+BastienHue@users.noreply.github.com>
2020-05-27 09:48:11 +0200

    Add files via upload

---
+++ chataccess.php
@@ -0,0 +1,19 @@
+<?php
+// Connexion à la base de données
+try
+{
+	$bdd = new PDO('mysql:host=localhost;dbname=MiniChat;charset=utf8', 'skohl', 'Samurai-83');
+}
+catch(Exception $e)
+{
+        die('Erreur : '.$e->getMessage());
+}
+
+// Insertion du message à l'aide d'une requête préparée
+$req = $bdd->prepare('INSERT INTO `chat_thread` (`pseudo`, `message`, `date_publication`) VALUES(?,?, NOW())');
+$req->execute(array($_POST['pseudo'], $_POST['message']));
+
+// Redirection du visiteur vers la page du minichat
+header('Location: index.php');
+?>
+	
---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<title>Minichat 2.0</title>
+
+</head>
+<body>
+	
+	<p><strong>Veuillez saisir votre pseudo et votre message : </strong></p>
+	<form action="/minichat2/chataccess.php" method="post">
+		<p>Pseudo : </p>
+		<p><input type="text" name="pseudo" value=""></p>
+		<p>Message : </p>
+		<p><input type="text" name="message" value=""></p>
+		<!--<p><strong>Vous avez saisi toutes les informations demandées, cliquez ci-dessous: </strong></p>-->
+		<p><input type="submit" value="Confirmer"/></p>
+	</form>
+
+	<?php 
+	try {
+		$bdd = new PDO('mysql:host=localhost;dbname=MiniChat;charset=utf8', 'skohl', 'Samurai-83');
+	}
+	catch(exception $e)
+	{
+		die('Erreur : '.$e->getMessage());
+	}
+	
+	$reponse=$bdd->query('SELECT `pseudo`,`message`,`date_publication` FROM chat_thread ORDER BY ID DESC LIMIT 0,10');
+
+	while ($donnees = $reponse->fetch()) 
+	{
+		echo '<p><strong>'.htmlspecialchars($donnees['pseudo']).'</strong> ('.htmlspecialchars($donnees['date_publication']).') : '.htmlspecialchars($donnees['message']).'</p>';
+	}
+
+	$reponse->closeCursor();
+
+	?>
+
+
+
+
+
+</body>
+</html>
